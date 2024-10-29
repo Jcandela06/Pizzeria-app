12b31f698d935c5f136b824f2506f63c9bbad9c5
Juan David <juandm417@gmail.com>
2024-10-29 16:01:13 -0500

    se arreglan algunos aspectos de el modelo y el controlador

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'client_id',
+        'branch_id',
+        'total_price',
+        'status',
+        'delivery_type',
+        'delivery_person_id'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function deliveryPerson()
+    {
+        return $this->belongsTo(Employee::class, 'delivery_person_id');
+    }
 }
