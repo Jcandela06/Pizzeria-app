99b78d43e25a4ebe941e7db56aa39d75b05f7370
Alejandro Muñoz <alejandromunozf005@gmail.com>
2024-11-19 22:14:38 -0500

    Api Pizza,size,ingredient

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\api\ClientController;
 use App\Http\Controllers\api\EmployeeController;
+use App\Http\Controllers\api\IngredientController;
+use App\Http\Controllers\api\Pizza_IngredientController;
+use App\Http\Controllers\api\Pizza_SizeController;
 use App\Http\Controllers\api\PizzaController;
 use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
@@ -15,3 +18,6 @@ Route::apiResource('users', UserController::class);
 Route::apiResource('clients', ClientController::class);
 Route::apiResource('employees', EmployeeController::class);
 Route::apiResource('pizzas', PizzaController::class);
+Route::apiResource('pizza_size', Pizza_SizeController::class);
+Route::apiResource('ingredients', IngredientController::class);
+Route::apiResource('pizza_ingredient', Pizza_IngredientController::class);
